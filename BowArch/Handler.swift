8ed31b0bfc33aac6e868ab91080f47057f52e9ec
Tomás <truizlop@gmail.com>
2020-03-16 13:00:14 +0100

    Add utility functions to Handler

--- BowArch/Handler.swift
+++ BowArch/Handler.swift
@@ -17,6 +17,22 @@ public class EffectHandler<Eff: Async, M: Monad, Environment, Input> {
             self.f(kleisli.contramap(f))
         }
     }
+    
+    public func noOp() -> Kleisli<Eff, Environment, [Input]> {
+        send(action: .pure(()))
+    }
+    
+    public func send(action: Kind<M, Void>) -> Kleisli<Eff, Environment, [Input]> {
+        send(effect: Eff.pure(action))
+    }
+    
+    public func send(effect: Kind<Eff, Kind<M, Void>>) -> Kleisli<Eff, Environment, [Input]> {
+        send(effect: Kleisli { _ in effect })
+    }
+    
+    public func send(effect: Kleisli<Eff, Environment, Kind<M, Void>>) -> Kleisli<Eff, Environment, [Input]> {
+        Kleisli { _ in self.handle(effect).as([]) }
+    }
 }
 
 public extension EffectHandler where Environment == Any {

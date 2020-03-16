1b750f9fbdf1646ad43ae647c710925bea81e008
Tomás <truizlop@gmail.com>
2020-03-16 12:26:36 +0100

    Add function to transform the Environment of a Handler

--- BowArch/Handler.swift
+++ BowArch/Handler.swift
@@ -11,4 +11,10 @@ public class EffectHandler<Eff: Async, M: Monad, Environment, Input> {
     public func handle(_ env: Kleisli<Eff, Environment, Kind<M, Void>>) -> Kind<Eff, Void> {
         f(env)
     }
+    
+    public func map<LocalEnvironment>(_ f: @escaping (Environment) -> LocalEnvironment) -> EffectHandler<Eff, M, LocalEnvironment, Input> {
+        EffectHandler<Eff, M, LocalEnvironment, Input> { kleisli in
+            self.f(kleisli.contramap(f))
+        }
+    }
 }

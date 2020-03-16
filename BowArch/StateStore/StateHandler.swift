6a9503ade1eb6611ca762ce11e415456f22d9af4
Tomás <truizlop@gmail.com>
2020-03-16 13:13:53 +0100

    Add convenience functions to change the focus of a Handler

--- BowArch/StateStore/StateHandler.swift
+++ BowArch/StateStore/StateHandler.swift
@@ -15,4 +15,50 @@ public extension EffectStateTHandler {
             { eff in eff.map { state in state^.focus(lens) }^ } >>> self.handle
         ).map(getter.get)
     }
+    
+    func focus<MM: Monad, LocalState, GlobalState, LocalEnvironment, LocalInput>(
+        _ f: @escaping (Environment) -> LocalEnvironment,
+        _ lens: Lens<GlobalState, LocalState>
+    ) -> EffectStateTHandler<Eff, MM, LocalEnvironment, LocalState, LocalInput>
+        where M == StateTPartial<MM, GlobalState> {
+        focus(Getter(get: f), lens)
+    }
+    
+    func focus<MM: Monad, LocalState, GlobalState, LocalEnvironment, LocalInput>(
+        _ keyPath: KeyPath<Environment, LocalEnvironment>,
+        _ lens: Lens<GlobalState, LocalState>
+    ) -> EffectStateTHandler<Eff, MM, LocalEnvironment, LocalState, LocalInput>
+        where M == StateTPartial<MM, GlobalState> {
+        focus({ x in x[keyPath: keyPath] }, lens)
+    }
+    
+    func focus<MM: Monad, LocalState, GlobalState: AutoLens, LocalEnvironment, LocalInput>(
+        _ getter: Getter<Environment, LocalEnvironment>,
+        _ keyPath: WritableKeyPath<GlobalState, LocalState>
+    ) -> EffectStateTHandler<Eff, MM, LocalEnvironment, LocalState, LocalInput>
+        where M == StateTPartial<MM, GlobalState> {
+        focus(getter, GlobalState.lens(for: keyPath))
+    }
+
+    func focus<MM: Monad, LocalState, GlobalState: AutoLens, LocalEnvironment, LocalInput>(
+        _ keyPath: KeyPath<Environment, LocalEnvironment>,
+        _ writableKeyPath: WritableKeyPath<GlobalState, LocalState>
+    ) -> EffectStateTHandler<Eff, MM, LocalEnvironment, LocalState, LocalInput>
+        where M == StateTPartial<MM, GlobalState> {
+        focus(keyPath, GlobalState.lens(for: writableKeyPath))
+    }
+    
+    func focus<MM: Monad, LocalState, GlobalState, LocalInput>(
+        _ lens: Lens<GlobalState, LocalState>
+    ) -> EffectStateTHandler<Eff, MM, Environment, LocalState, LocalInput>
+        where M == StateTPartial<MM, GlobalState> {
+        focus(.identity, lens)
+    }
+    
+    func focus<MM: Monad, LocalState, GlobalState: AutoLens, LocalInput>(
+        _ keyPath: WritableKeyPath<GlobalState, LocalState>
+    ) -> EffectStateTHandler<Eff, MM, Environment, LocalState, LocalInput>
+        where M == StateTPartial<MM, GlobalState> {
+        focus(.identity, keyPath)
+    }
 }

d5c09dac0571298ecdc0a19dc40d87da27457eb2
Tomás <truizlop@gmail.com>
2020-03-16 12:23:56 +0100

    Add functions to lift a UI to a different Monad

--- BowArch/UI.swift
+++ BowArch/UI.swift
@@ -7,4 +7,16 @@ public final class UI<Eff: Async, M: Monad, A> {
     public init(_ makeView: @escaping (@escaping (Kind<Eff, Kind<M, Void>>) -> Kind<Eff, Void>) -> A) {
         self.makeView = makeView
     }
+    
+    public func lift<MM: Monad>(_ f: @escaping (Kind<M, Void>) -> Kind<MM, Void>) -> UI<Eff, MM, A> {
+        UI<Eff, MM, A> { send in
+            self.makeView { action in send(action.map(f)) }
+        }
+    }
+    
+    public func lift<MM: Monad>(_ f: @escaping (Kind<Eff, Kind<M, Void>>) -> Kind<Eff, Kind<MM, Void>>) -> UI<Eff, MM, A> {
+        UI<Eff, MM, A> { send in
+            self.makeView { action in send(f(action)) }
+        }
+    }
 }

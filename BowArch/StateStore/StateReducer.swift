8225371a71c95b605757c8b80064acce34cd555e
Tomás <truizlop@gmail.com>
2020-03-16 13:14:58 +0100

    Use optics to change focus of a Reducer

--- BowArch/StateStore/StateReducer.swift
+++ BowArch/StateStore/StateReducer.swift
@@ -4,3 +4,22 @@ import BowEffects
 
 public typealias EffectStateTReducer<Eff: Async & UnsafeRun, M: Monad, Environment, S, Input> = EffectReducer<Eff, StateTPartial<M, S>, Environment, Input>
 public typealias EffectStateReducer<Eff: Async & UnsafeRun, Environment, S, Input> = EffectStateTReducer<Eff, ForId, Environment, S, Input>
+
+public extension EffectStateTReducer {
+    func focus<MM: Monad, GlobalState, LocalState, GlobalEnvironment, GlobalInput>(
+        _ getter: Getter<GlobalEnvironment, Environment>,
+        _ lens: Lens<GlobalState, LocalState>,
+        _ prism: Prism<GlobalInput, Input>
+    ) -> EffectStateTReducer<Eff, MM, GlobalEnvironment, GlobalState, GlobalInput>
+        where M == StateTPartial<MM, LocalState> {
+        EffectStateTReducer<Eff, MM, GlobalEnvironment, GlobalState, GlobalInput> { input, handler in
+            if let newInput = prism.getOptional(input) {
+                return self.reduce(newInput, handler.focus(getter, lens)).map { array in
+                    array.map(prism.reverseGet)
+                    }^.contramap(getter.get)
+            } else {
+                return Kleisli.pure([])^
+            }
+        }
+    }
+}

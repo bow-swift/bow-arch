d9bf0f39bb33b97ea17134c0f69084e0e4c77449
Tomás <truizlop@gmail.com>
2020-03-16 12:30:37 +0100

    Add binding between reducer and handler

--- BowArch/Reducer.swift
+++ BowArch/Reducer.swift
@@ -14,4 +14,25 @@ public struct EffectReducer<Eff: Async & UnsafeRun, M: Monad, Environment, Input
     ) -> Kleisli<Eff, Environment, [Input]> {
         self.f(input, handler)
     }
+    
+    public func sendingTo(
+        _ handler: EffectHandler<Eff, M, Environment, Input>,
+        environment: Environment
+    ) -> (Input) -> Void {
+        { input in
+            self.f(input, handler)
+                .flatMap { inputs in self.reflow(inputs, handler) }^
+                .run(environment)
+                .runNonBlocking(on: .global(qos: .background))
+        }
+    }
+    
+    private func reflow(
+        _ inputs: [Input],
+        _ handler: EffectHandler<Eff, M, Environment, Input>
+    ) -> Kleisli<Eff, Environment, Void> {
+        inputs.isEmpty ? Kleisli.pure(())^
+            : inputs.flatTraverse { input in self.reduce(input, handler) }^
+                .flatMap { inputs in self.reflow(inputs, handler) }^
+    }
 }

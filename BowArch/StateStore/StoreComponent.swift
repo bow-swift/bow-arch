fed383fcca88b3e0d7964539cc7bc33279d5519e
Tomás <truizlop@gmail.com>
2020-03-16 13:17:04 +0100

    Add convenience initializer for component with StateT-StoreT

--- BowArch/StateStore/StoreComponent.swift
+++ BowArch/StateStore/StoreComponent.swift
@@ -4,3 +4,31 @@ import BowEffects
 
 public typealias EffectStoreTComponent<Eff: Async, WW: Comonad, MM: Monad, S, V: View> = EffectComponentView<Eff, StoreTPartial<S, WW>, StateTPartial<MM, S>, V>
 public typealias EffectStoreComponent<Eff: Async, S, V: View> = EffectStoreTComponent<Eff, ForId, ForId, S, V>
+
+public extension EffectStoreTComponent {
+    init<E, A, I, WW: Comonad & Applicative, MM: Monad>(
+        initialState: A,
+        environment: E,
+        pairing: Pairing<MM, WW>,
+        render: @escaping (A, EffectStateTHandler<Eff, MM, E, A, I>) -> V)
+        where W == StoreTPartial<A, WW>,
+              M == StateTPartial<MM, A> {
+        self.init(StoreT(initialState, WW.pure({ state in
+            UI { send in
+                render(state, EffectStateTHandler(send).map(constant(environment)))
+            }
+        })), pairing)
+    }
+    
+    init<A, WW: Comonad & Applicative, MM: Monad, I>(
+        initialState: A,
+        pairing: Pairing<MM, WW>,
+        render: @escaping (A, EffectStateTHandler<Eff, MM, Any, A, I>) -> V)
+        where W == StoreTPartial<A, WW>,
+              M == StateTPartial<MM, A> {
+        self.init(initialState: initialState,
+                  environment: () as Any,
+                  pairing: pairing,
+                  render: render)
+    }
+}

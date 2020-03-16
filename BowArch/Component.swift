ce3c564fe17bfdf497fd6a072c202c263b95b120
Tomás <truizlop@gmail.com>
2020-03-16 12:40:43 +0100

    Add functions to explore a Component and extract the view

--- BowArch/Component.swift
+++ BowArch/Component.swift
@@ -10,6 +10,20 @@ public final class EffectComponent<Eff: Async, W: Comonad, M: Monad, A>: Observa
         self.wui = component
         self.pairing = pairing
     }
+    
+    public func explore() -> A {
+        self.explore { c in
+            Eff.later(.main) { self.wui = c.wui }
+        }
+    }
+    
+    public func explore(_ write: @escaping (EffectComponent<Eff, W, M, A>) -> Kind<Eff, Void>) -> A {
+        self.wui.extract().makeView({ base in
+            base.flatMap { action in
+                write(EffectComponent(self.pairing.select(action, self.wui.duplicate()), self.pairing))
+            }
+        })
+    }
 }
 
 public func ==<Eff: Monad, W: Comonad, M: Monad, A>(

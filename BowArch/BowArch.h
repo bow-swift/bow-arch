d4e7abeb423f64bc0bf8e1eee4a7688942621be9
Tomás <truizlop@gmail.com>
2020-03-16 12:04:04 +0100

    Initial Commit

---
+++ BowArch/BowArch.h
@@ -0,0 +1,19 @@
+//
+//  BowArch.h
+//  BowArch
+//
+//  Created by Tomás Ruiz López on 16/03/2020.
+//  Copyright © 2020 The Bow Authors. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BowArch.
+FOUNDATION_EXPORT double BowArchVersionNumber;
+
+//! Project version string for BowArch.
+FOUNDATION_EXPORT const unsigned char BowArchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BowArch/PublicHeader.h>
+
+

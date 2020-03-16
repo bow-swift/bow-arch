dab9e66589dc824a36c82bebdc5922c598ed0f1d
Tomás <truizlop@gmail.com>
2020-03-16 13:13:12 +0100

    Use optics to change focus of a Handler

--- BowArch/StateStore/StateHandler.swift
+++ BowArch/StateStore/StateHandler.swift
@@ -4,3 +4,15 @@ import BowOptics
 
 public typealias EffectStateTHandler<Eff: Async, M: Monad, Environment, State, Input> = EffectHandler<Eff, StateTPartial<M, State>, Environment, Input>
 public typealias EffectStateHandler<Eff: Async, Environment, State, Input> = EffectStateTHandler<Eff, ForId, Environment, State, Input>
+
+public extension EffectStateTHandler {
+    func focus<MM: Monad, LocalState, GlobalState, LocalEnvironment, LocalInput>(
+        _ getter: Getter<Environment, LocalEnvironment>,
+        _ lens: Lens<GlobalState, LocalState>
+    ) -> EffectStateTHandler<Eff, MM, LocalEnvironment, LocalState, LocalInput>
+        where M == StateTPartial<MM, GlobalState> {
+        EffectStateTHandler(
+            { eff in eff.map { state in state^.focus(lens) }^ } >>> self.handle
+        ).map(getter.get)
+    }
+}

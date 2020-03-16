d23b69ada5b95e1eb3ea6309ed2a1467210a7e22
Tomás <truizlop@gmail.com>
2020-03-16 12:45:43 +0100

    Add functions to react on actions with effects

--- BowArch/ComponentView.swift
+++ BowArch/ComponentView.swift
@@ -12,4 +12,12 @@ public struct EffectComponentView<Eff: Async, W: Comonad, M: Monad, V: View>: Vi
     public var body: some View {
         component.explore()
     }
+    
+    public func onEffect(_ eff: @escaping (EffectComponent<Eff, W, M, V>) -> Kind<Eff, Void>) -> EffectComponentView<Eff, W, M, V> {
+        EffectComponentView(self.component.onEffect(eff))
+    }
+    
+    public func onEffectAction(_ eff: @escaping (EffectComponent<Eff, W, M, V>, Kind<M, Void>) -> Kind<Eff, Void>) -> EffectComponentView<Eff, W, M, V> {
+        EffectComponentView(self.component.onEffectAction(eff))
+    }
 }

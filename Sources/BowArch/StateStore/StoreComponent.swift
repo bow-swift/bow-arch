bfcd3d1c1427a75c18c5d9674fe51cd423032215
Tomás <truizlop@gmail.com>
2020-03-19 09:48:36 +0100

    Add enhancements to test StoreComponent

--- Sources/BowArch/StateStore/StoreComponent.swift
+++ Sources/BowArch/StateStore/StoreComponent.swift
@@ -55,3 +55,19 @@ public extension EffectStoreComponent {
                   render: render)
     }
 }
+
+extension EffectStoreTComponent {
+    func storeT<A, WW: Comonad, MM: Monad>() -> StoreT<A, WW, UI<Eff, M, V>>
+        where W == StoreTPartial<A, WW>,
+              M == StateTPartial<MM, A> {
+        self.component.wui^
+    }
+}
+
+extension EffectStoreComponent {
+    func store<A>() -> Store<A, UI<Eff, M, V>>
+        where W == StorePartial<A>,
+              M == StatePartial<A> {
+        self.component.wui^
+    }
+}

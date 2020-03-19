7122e3003f54002d246e1abdc670a6fdbfead3c9
Tomás <truizlop@gmail.com>
2020-03-19 09:48:46 +0100

    Add enhancements to test TracedComponent

--- Sources/BowArch/WriterTraced/TracedComponent.swift
+++ Sources/BowArch/WriterTraced/TracedComponent.swift
@@ -52,3 +52,19 @@ public extension EffectTracedComponent {
                   render: render)
     }
 }
+
+extension EffectTracedTComponent {
+    func tracedT<A, WW: Comonad, MM: Monad>() -> TracedT<A, WW, UI<Eff, M, V>>
+        where W == TracedTPartial<A, WW>,
+              M == WriterTPartial<MM, A> {
+        self.component.wui^
+    }
+}
+
+extension EffectStoreComponent {
+    func traced<A>() -> Traced<A, UI<Eff, M, V>>
+        where W == TracedPartial<A>,
+              M == WriterPartial<A> {
+        self.component.wui^
+    }
+}

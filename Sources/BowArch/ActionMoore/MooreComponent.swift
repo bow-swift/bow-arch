084bc55477d1d91b4993d8f2a81601e5e439f150
Tomás <truizlop@gmail.com>
2020-03-24 11:13:16 +0100

    Add convenience initializer for MooreComponent

--- Sources/BowArch/ActionMoore/MooreComponent.swift
+++ Sources/BowArch/ActionMoore/MooreComponent.swift
@@ -3,3 +3,22 @@ import Bow
 import BowEffects
 
 public typealias EffectMooreComponent<Eff: Async, Action, V: View> = EffectComponentView<Eff, MoorePartial<Action>, ActionPartial<Action>, V>
+
+public extension EffectMooreComponent {
+    init<E, A, S, I>(
+        initialState: S,
+        environment: E,
+        reducer: Reducer<S, A>,
+        render: @escaping (S, EffectActionHandler<Eff, E, A, I>) -> V)
+        where W == MoorePartial<A>,
+              M == ActionPartial<A> {
+    self.init(Moore.from(
+        initialState: initialState,
+        render: { state in
+            UI { send in
+                render(state, EffectActionHandler(send, environment: environment))
+            }
+    },
+        update: reducer.run))
+    }
+}

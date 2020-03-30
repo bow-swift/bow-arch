72f0ea308bd1909b93efc9e90e911ccf937333a7
Tomás <truizlop@gmail.com>
2020-03-30 10:15:37 +0200

    Add default action dispatcher

--- Sources/BowArch/ActionMoore/ActionDispatcher.swift
+++ Sources/BowArch/ActionMoore/ActionDispatcher.swift
@@ -2,3 +2,11 @@ import Bow
 import BowEffects
 
 public typealias EffectActionDispatcher<Eff: Async & UnsafeRun, Environment, Action, Input> = EffectDispatcher<Eff, ActionPartial<Action>, Environment, Input>
+
+public extension EffectActionDispatcher where M == ActionPartial<Input> {
+    static func `default`() -> EffectActionDispatcher<Eff, Environment, Input, Input> {
+        EffectActionDispatcher { input, handler in
+            handler.send(action: Action.from(input))
+        }
+    }
+}

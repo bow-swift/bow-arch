7ad70a307774c3b8d60cfbb96b2c5f493efcd882
Tomás <truizlop@gmail.com>
2020-04-28 10:07:52 +0200

    Add narrow method

--- Sources/BowArch/StateStore/StateHandler.swift
+++ Sources/BowArch/StateStore/StateHandler.swift
@@ -5,13 +5,13 @@ import BowOptics
 public typealias EffectStateTHandler<Eff: Async, M: Monad, State> = EffectHandler<Eff, StateTPartial<M, State>>
 public typealias EffectStateHandler<Eff: Async, State> = EffectStateTHandler<Eff, ForId, State>
 
-public extension EffectStateTHandler {
-    func focus<MM: Monad, S, SS>(
-        _ lens: Lens<SS, S>
-    ) -> EffectStateTHandler<Eff, MM, S>
-        where M == StateTPartial<MM, SS> {
-        self.lift { state in
-            state^.focus(lens)
+public extension EffectHandler {
+    func narrow<S, S2>(
+        _ lens: Lens<S, S2>
+    ) -> EffectStateHandler<Eff, S2>
+        where M == StatePartial<S> {
+        self.lift { action in
+            action^.focus(lens)
         }
     }
 }

7468c9db4ec29c463268b24515f31dc548f1fb7c
Tomás <truizlop@gmail.com>
2020-04-29 13:10:27 +0200

    Add convenience methods

--- Sources/BowArch/StateStore/StoreComponent.swift
+++ Sources/BowArch/StateStore/StoreComponent.swift
@@ -13,7 +13,7 @@ public struct EffectStoreComponent<Eff: Async & UnsafeRun, E, S, I, V: View>: Vi
     public init(
         initialState: S,
         environment: E,
-        dispatcher: EffectStateDispatcher<Eff, E, S, I>,
+        dispatcher: EffectStateDispatcher<Eff, E, S, I> = .empty(),
         render: @escaping (S, @escaping (I) -> Void) -> V
     ) {
         self.init(
@@ -26,7 +26,7 @@ public struct EffectStoreComponent<Eff: Async & UnsafeRun, E, S, I, V: View>: Vi
     public init(
         initialState: S,
         environment: E,
-        dispatcher: EffectStateDispatcher<Eff, E, S, I>,
+        dispatcher: EffectStateDispatcher<Eff, E, S, I>  = .empty(),
         render: @escaping (S, @escaping (I) -> Void, EffectStateHandler<Eff, S>) -> V
     ) {
         self.initialState = initialState
@@ -70,8 +70,83 @@ public struct EffectStoreComponent<Eff: Async & UnsafeRun, E, S, I, V: View>: Vi
             })
     }
     
+    public func lift<E2>(
+        environment: E2,
+        transformEnvironment f: @escaping (E2) -> E
+    ) -> EffectStoreComponent<Eff, E2, S, I, V> {
+        self.lift(
+            initialState: self.initialState,
+            environment: environment,
+            transformEnvironment: f,
+            transformState: Lens.identity,
+            transformInput: Prism.identity)
+    }
+    
+    public func lift<S2>(
+        initialState: S2,
+        transformState lens: Lens<S2, S>
+    ) -> EffectStoreComponent<Eff, E, S2, I, V> {
+        self.lift(
+            initialState: initialState,
+            environment: self.environment,
+            transformEnvironment: { $0 },
+            transformState: lens,
+            transformInput: Prism.identity)
+    }
+    
+    public func lift<I2>(
+        transformInput prism: Prism<I2, I>
+    ) -> EffectStoreComponent<Eff, E, S, I2, V> {
+        self.lift(
+            initialState: self.initialState,
+            environment: self.environment,
+            transformEnvironment: { $0 },
+            transformState: Lens.identity,
+            transformInput: prism)
+    }
+    
+    public func lift<E2, S2>(
+        initialState: S2,
+        environment: E2,
+        transformEnvironment f: @escaping (E2) -> E,
+        transformState lens: Lens<S2, S>
+    ) -> EffectStoreComponent<Eff, E2, S2, I, V> {
+        self.lift(
+            initialState: initialState,
+            environment: environment,
+            transformEnvironment: f,
+            transformState: lens,
+            transformInput: Prism.identity)
+    }
+    
+    public func lift<E2, I2>(
+        environment: E2,
+        transformEnvironment f: @escaping (E2) -> E,
+        transformInput prism: Prism<I2, I>
+    ) -> EffectStoreComponent<Eff, E2, S, I2, V> {
+        self.lift(
+            initialState: self.initialState,
+            environment: environment,
+            transformEnvironment: f,
+            transformState: Lens.identity,
+            transformInput: prism)
+    }
+    
+    public func lift<S2, I2>(
+        initialState: S2,
+        transformState lens: Lens<S2, S>,
+        transformInput prism: Prism<I2, I>
+    ) -> EffectStoreComponent<Eff, E, S2, I2, V> {
+        self.lift(
+            initialState: initialState,
+            environment: self.environment,
+            transformEnvironment: { $0 },
+            transformState: lens,
+            transformInput: prism)
+    }
+    
     public func using(
-        dispatcher: EffectStateDispatcher<Eff, E, S, I>,
+        dispatcher: EffectStateDispatcher<Eff, E, S, I> = .empty(),
         handler: EffectStateHandler<Eff, S>
     ) -> EffectStoreComponent<Eff, E, S, I, V> {
         EffectStoreComponent(
@@ -90,5 +165,19 @@ public struct EffectStoreComponent<Eff: Async & UnsafeRun, E, S, I, V: View>: Vi
 public extension EffectStoreComponent {
     func store() -> Store<S, UI<Eff, StatePartial<S>, V>>{
         self.componentView.component.wui^
+    }
+}
+
+public extension EffectStoreComponent where E == Any {
+    init(
+        initialState: S,
+        dispatcher: EffectStateDispatcher<Eff, Any, S, I> = .empty(),
+        render: @escaping (S, @escaping (I) -> Void) -> V
+    ) {
+        self.init(
+            initialState: initialState,
+            environment: (),
+            dispatcher: dispatcher,
+            render: { s, h, _ in render(s, h) })
     }
 }

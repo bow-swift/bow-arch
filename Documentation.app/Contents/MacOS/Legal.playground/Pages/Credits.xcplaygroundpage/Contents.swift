6848e48160d3cf8054d2bd06e961e2c6ec7ce257
Tomás <truizlop@gmail.com>
2020-05-16 18:40:41 +0200

    Update Credits

--- Documentation.app/Contents/MacOS/Legal.playground/Pages/Credits.xcplaygroundpage/Contents.swift
+++ Documentation.app/Contents/MacOS/Legal.playground/Pages/Credits.xcplaygroundpage/Contents.swift
@@ -11,5 +11,7 @@
  
  For a comprehensive list of contributors, [visit the repository](https://github.com/bow-swift/bow-arch/graphs/contributors) on GitHub.
  
+ We want to thank Arthur Xavier, Phil Freeman, and Edward Kmett, for their previous work on Comonads and Comonadic UIs in the PureScript and Haskell languages. Their work has inspired the creation of this library.
+ 
  Bow Arch is supported by [47 Degrees](https://www.47deg.com).
  */

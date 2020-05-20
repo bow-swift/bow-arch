7a8eef8e7551fa55f2567665e06adf554d11e2bf
Miguel Ángel Díaz <madiaz.dev@gmail.com>
2020-05-20 12:14:59 +0200

    migrate xcodeproj to Swift Package (autogenerate)
    upate gitignore
    
    minor changes

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.2
 
 import PackageDescription
 
@@ -12,14 +12,17 @@ let package = Package(
     ],
 
     dependencies: [
-        .package(url: "https://github.com/bow-swift/bow.git", .branch("master")),
+        .package(name: "Bow", url: "https://github.com/bow-swift/bow.git", .exact("0.8.0")),
     ],
 
     targets: [
         // Library targets
-        .target(name: "BowArch", dependencies: ["Bow", "BowEffects", "BowOptics"]),
+        .target(name: "BowArch",
+                dependencies: [.product(name: "Bow", package: "Bow"),
+                               .product(name: "BowEffects", package: "Bow"),
+                               .product(name: "BowOptics", package: "Bow")]),
         // Test targets
         .testTarget(name: "BowArchTests",
-                    dependencies: ["BowArch"])
+                    dependencies: [.target(name: "BowArch")])
     ]
 )

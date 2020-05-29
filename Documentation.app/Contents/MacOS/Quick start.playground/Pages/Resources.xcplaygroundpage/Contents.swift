3e8162128a92dfb2170823776641933c67f3f667
Tomás <truizlop@gmail.com>
2020-05-29 10:23:30 +0200

    Update acknowledgements and add resources

--- Documentation.app/Contents/MacOS/Quick start.playground/Pages/Resources.xcplaygroundpage/Contents.swift
+++ Documentation.app/Contents/MacOS/Quick start.playground/Pages/Resources.xcplaygroundpage/Contents.swift
@@ -6,11 +6,12 @@
 // nef:end
 /*:
  # Resources
+ 
  In this section you can find a list of projects, libraries, presentations or blog posts that use Bow Arch:
  
  ## GitHub projects
  
- Be the first one showing here!
+ - [nef Playgrounds for iPad](https://github.com/bow-swift/nef-editor-client): An iPad application to create Swift Playgrounds with 3rd party dependencies.
  
  ## Libraries
  
@@ -22,6 +23,6 @@
  
  ## Blog posts
  
- Be the first one showing here!
+ - [Introducing Bow Arch 0.1.0](https://www.47deg.com/blog/bow-arch-0-1-0-release/)
  
  */
